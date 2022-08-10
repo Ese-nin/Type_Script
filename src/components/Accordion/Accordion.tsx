6de8550e3732b5d1e390a738a4905df79c7b7f64
Сергей <privet15.89@mail.ru>
2022-08-10 12:57:32 +0500

    Add Accordion.tsx

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+type AccordionPropsType = {
+    titleValue: string;
+    collapsed: boolean;
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log("Accordion rendering")
+    if (props.collapsed === false) {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+                <AccordionBody/>
+            </div>
+        )
+    } else return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string;
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering")
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody(props: any) {
+    console.log("AccordionBody rendering")
+    return (
+        <div>
+            <ul>
+                <li>2</li>
+                <li>15</li>
+                <li>46</li>
+                <li>127</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Accordion;
