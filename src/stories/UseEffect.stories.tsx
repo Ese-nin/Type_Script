bf6f90184e218d334cda68b68fafb9867dc64c7b
Сергей <privet15.89@mail.ru>
2022-10-19 18:31:26 +0500

    useEffect: clock

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -33,3 +33,60 @@ export const Example = () => {
     </>
 
 }
+
+
+export const SetTimeoutExample = () => {
+    console.log("SetTimeoutExample")
+    const [count, setCount] = useState(0)
+    const [fake, setFake] = useState(0)
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log('tick: ' + count)
+            setCount(state => state + 1)
+        }, 1000)
+
+    }, [])
+
+
+    return <>
+        {/*<button onClick={() => setCount(state => state + 1)}>count +</button>*/}
+        count: {count}
+        <br/>
+        {/*<button onClick={() => setFake(state => state + 1)}>fake +</button>*/}
+        fake: {fake}
+    </>
+
+}
+
+
+export const ClockExample = () => {
+    console.log("ClockExample")
+    const [date, setDate] = useState(new Date())
+
+    const setTimeForHuman = (num: number) => {
+       return  num < 10 ? '0' + num : num
+    }
+
+    let hours = setTimeForHuman(date.getHours())
+    let minutes = setTimeForHuman(date.getMinutes())
+    let seconds = setTimeForHuman(date.getSeconds())
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+    }, [])
+
+
+    return <>
+        clock: {hours}:{minutes}:{seconds}
+        <br/>
+
+    </>
+
+}
